0b8465c5f9c560526ef9da284157c25b55b902af
Arnaud Mucyo <arnmuc98@gmail.com>
2022-06-09 09:48:06 +0200

    Initial commit

---
+++ test2.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+    $(function loadElements(){
+    $(".name").slideDown(7000);
+    $(".dev").slideDown(2000);
+    $(".welcome").slideDown(3000);
+    
+    
+    })
+
+  
+    
+    
+    });
